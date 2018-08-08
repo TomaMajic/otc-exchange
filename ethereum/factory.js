9e8e232a265ff07e95383e8d558a7d455a48d04f
TomaMajic <tomamajic111@gmail.com>
2018-08-08 14:15:16 +0200

    Complete factory.js with deployed address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from './web3';
 import AgentFactory from './build/contracts/AgentFactory.json';
 
 const abi = AgentFactory.abi;
-const address = 'address';
+const address = '0x067de8a0bcc3c3c5520dc17bb1f0726d985305cc';
 
 const instance = new web3.eth.Contract(
 	abi,
