58e6841ee018abc0e96c730e642725491444a2b2
TomaMajic <tomamajic111@gmail.com>
2018-08-23 08:08:46 +0200

    Initialize exchanges new page

---
+++ pages/exchanges/new.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ExchangesNew extends React.Component {
+
+	render() {
+		return (
+			<h3>Create a new exchange</h3>
+		);
+	}
+}
+
+export default ExchangesNew;
